8a1acac543c8887d60580c7f05f3395ecdee2e65
BrettMerri <the.b.merri@gmail.com>
2017-04-27 10:09:32 -0400

    initial commit

---
+++ ConsoleMinesweeper/Board.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMinesweeper
+{
+    class Board
+    {
+        private int horizontal;
+        private int vertical;
+        private int mines;
+
+        public Board(int horizontal, int vertical, int mines)
+        {
+            this.horizontal = horizontal;
+            this.vertical = vertical;
+            this.mines = mines;
+        }
+
+        public int Horizontal
+        {
+            get
+            {
+                return horizontal;
+            }
+
+            set
+            {
+                horizontal = value;
+            }
+        }
+
+        public int Vertical
+        {
+            get
+            {
+                return vertical;
+            }
+
+            set
+            {
+                vertical = value;
+            }
+        }
+
+        public int Mines
+        {
+            get
+            {
+                return mines;
+            }
+
+            set
+            {
+                mines = value;
+            }
+        }
+    }
+}
