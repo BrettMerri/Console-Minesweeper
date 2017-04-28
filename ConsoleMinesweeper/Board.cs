3097fa32e572dbd1102181216b118b4d534a2fd9
BrettMerri <the.b.merri@gmail.com>
2017-04-27 23:33:05 -0400

    Seperated the CreateBoard method into seperate methods for better code structure

--- ConsoleMinesweeper/Board.cs
+++ ConsoleMinesweeper/Board.cs
@@ -13,7 +13,10 @@ namespace ConsoleMinesweeper
         private int vertical;
         private int mines;
 
-        public Board() { }
+        public Board()
+        {
+            //All board variables will come from EasyBoard.cs, MediumBoard.cs, HardBoard.cs, or CustomerBoard.cs
+        }
 
         public void CreateBoard(int[,] boardArray, bool[,] unavailableCells)
         {
@@ -32,69 +35,53 @@ namespace ConsoleMinesweeper
             //Print new line before printing the board
             Console.WriteLine(); 
 
-            //Outer for loop: i = row
-            for (int i = 0; i < vertical; i++)
+            //Outer for loop: rows
+            for (int row = 0; row < vertical; row++)
             {
-                //If a 2-digit Y axis exists, print all the single digit axis with an extra space at the end so the axis aligns with the board
-                if (twoDigitYAxis == true && vertical - i < 10)
-                {
-                    //Prints Y-axis coordinants < 10 with two spaces at the end (1 digit + 2 spaces = 3 characters total)
-                    Console.Write($"{vertical - i}  ");
-                }
-                else
-                {
-                    //Prints Y-axis coordinants > 10 with one space at the end (2 digits + 1 space = 3 characters total)
-                    Console.Write($"{vertical - i} ");
-                }
+                //Writes the Y Axis coordinants for this row to the console
+                WriteYAxisCoodinants(row, twoDigitYAxis);
 
-                //Set console text color to White before printing the board
-                Console.ForegroundColor = ConsoleColor.White;
+                //Writes the board values for this row
+                WriteBoardValues(unavailableCells, boardArray, row, twoDigitXAxis);
 
-                //Inner for loop: j = column
-                for (int j = 0; j < horizontal; j++)
-                {
-                    //j = the column index value
-                    //Vertical - i - 1 = the row index value
-                    //Checks if the value of the array is "not unavailable" (or available to be selected)
-                    if (unavailableCells[j, Vertical - i - 1] == false)
-                    {
-                        //Write "#" if the cell is availble to be selected
-                        Console.Write("#");
-                    }
-                    else
-                    {
-                        //Write the integer value of boardArray if the cell has been chosen already (or unavailable to be selected)
-                        Console.Write(boardArray[j, Vertical - i - 1].ToString());
-                    }
-
-                    //If there a 2-digit X axis exists, write the board with an extra space after each element to algin the board with the axis.
-                    if (twoDigitXAxis == true)
-                    {
-                        //Print board with two spaces after each element if 2-digit X axis values exist
-                        Console.Write("  "); 
-                    }
-                    else
-                    {
-                        //Print board with one space after each element if only 1-digit X axis values exist
-                        Console.Write(" "); 
-                    }
-
-                }
-                //Set console text color back to default (Gray) before printing the axis again
-                Console.ForegroundColor = ConsoleColor.Gray;
+                //Creates a new line before going on to the next row
                 Console.WriteLine();
             }
 
+            WriteXAxisCoordinants(twoDigitXAxis, twoDigitYAxis);
+
+            //Print two new lines after the board has printed.
+            Console.Write("\n\n"); 
+
+        }
+
+        public void WriteYAxisCoodinants(int row, bool twoDigitYAxis)
+        {
+            //If a 2-digit Y axis exists, print all the single digit axis with an extra space at the end so the axis aligns with the board
+            if (twoDigitYAxis == true && vertical - row < 10)
+            {
+                //Prints Y-axis coordinants < 10 with two spaces at the end (1 digit + 2 spaces = 3 characters total)
+                Console.Write($"{vertical - row}  ");
+            }
+            else
+            {
+                //Prints Y-axis coordinants > 10 with one space at the end (2 digits + 1 space = 3 characters total)
+                Console.Write($"{vertical - row} ");
+            }
+        }
+
+        public void WriteXAxisCoordinants(bool twoDigitXAxis, bool twoDigitYAxis)
+        {
             //If a 2-digit Y exists exists, write three-spaces before writing the X-axis to align with the board.
             if (twoDigitYAxis == true)
             {
                 //Write 3 spaces before the X-axis coodinants if 2-digit Y axis values exists
-                Console.Write("   "); 
+                Console.Write("   ");
             }
             else
             {
                 //2 space left padding before the x-axis coordinants
-                Console.Write("  "); 
+                Console.Write("  ");
             }
 
             for (int i = 0; i < horizontal; i++)
@@ -103,19 +90,55 @@ namespace ConsoleMinesweeper
                 if (twoDigitXAxis == true && i + 1 < 10)
                 {
                     //Prints single-digit X-axis coordinants with two spaces at the end
-                    Console.Write($"{i + 1}  "); 
+                    Console.Write($"{i + 1}  ");
                 }
                 else
                 {
                     //Prints two-digit X-axis coordinants with one space at the end
-                    Console.Write($"{i + 1} "); 
+                    Console.Write($"{i + 1} ");
+                }
+            }
+        }
+
+        public void WriteBoardValues(bool[,] unavailableCells, int[,] boardArray, int row,  bool twoDigitXAxis)
+        {
+            //Set console text color to White before printing the board
+            Console.ForegroundColor = ConsoleColor.White;
+
+            //Inner for loop: columns
+            for (int column = 0; column < horizontal; column++)
+            {
+                //j = the column index value
+                //Vertical - i - 1 = the row index value
+                //Checks if the value of the array is "not unavailable" (or available to be selected)
+                if (unavailableCells[column, vertical - row - 1] == false)
+                {
+                    //Write "#" if the cell is availble to be selected
+                    Console.Write("#");
+                }
+                else
+                {
+                    //Write the integer value of boardArray if the cell has been chosen already (or unavailable to be selected)
+                    Console.Write(boardArray[column, vertical - row - 1].ToString());
+                }
+
+                //If there a 2-digit X axis exists, write the board with an extra space after each element to algin the board with the axis.
+                if (twoDigitXAxis == true)
+                {
+                    //Print board with two spaces after each element if 2-digit X axis values exist
+                    Console.Write("  ");
+                }
+                else
+                {
+                    //Print board with one space after each element if only 1-digit X axis values exist
+                    Console.Write(" ");
                 }
             }
 
-            //Print two new lines after the board has printed.
-            Console.Write("\n\n"); 
-
+            //Set console text color back to default (Gray)
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
+
         public int[,] GenerateMines(int[,] emptyBoardArray)
         {
             //Creates new random object
